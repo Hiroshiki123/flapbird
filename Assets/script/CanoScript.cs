55fb4814fc26417aca1a98ff31a6f15451808fcd
Hiroshiki123 <79053749+Hiroshiki123@users.noreply.github.com>
2023-08-07 11:45:26 -0300

    flapbird

--- Assets/script/CanoScript.cs
+++ Assets/script/CanoScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CanoScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
+    public float limitX;
+    public float uplimit;
+    public float downlimit;
+    Vector2 initialPosition;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        //define a possição inicial como a posição do objeto
+        initialPosition = transform.position;
+        PipePosition(uplimit, downlimit);
+    }
     void Update()
     {
-        
+        //move o cano pra esqueda
+        transform.Translate(Vector2.left*speed*Time.deltaTime);
+        if (transform.position.x < limitX)
+        {
+            //volta para a posição inicial
+            PipePosition(uplimit, downlimit);
+        } 
+    }
+    void PipePosition(float u, float d)
+    {
+        transform.position = initialPosition;
+        transform.position = new Vector3(transform.position.x, Random.Range(u, d));
     }
 }
