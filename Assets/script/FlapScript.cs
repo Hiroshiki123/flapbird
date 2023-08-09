f1119c72d813e44708250dffc8b71d6497aa6bec
Hiroshiki123 <79053749+Hiroshiki123@users.noreply.github.com>
2023-08-09 09:13:03 -0300

    aaaaaa

--- Assets/script/FlapScript.cs
+++ Assets/script/FlapScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class FlapScript : MonoBehaviour
 {
+    public float impulso;
+
+    bool impulcionar;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            impulcionar = true;
+        }
+        transform.Rotate(Vector3.forward * (rb.velocity.y * 0.05f));
+    }
+    private void FixedUpdate()
+    {
+        if (impulcionar)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, impulso);
+            impulcionar=false;
+        }
     }
 }
