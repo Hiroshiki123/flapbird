e4d3f5a026b5dcc99d411adf0baadc703684b160
Hiroshiki123 <79053749+Hiroshiki123@users.noreply.github.com>
2023-08-16 09:37:39 -0300

    6

--- Assets/script/corrotina/corrotina.cs
+++ Assets/script/corrotina/corrotina.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class corrotina : MonoBehaviour
 {
+    public Text texto1;
+    public Text texto2;
+    public Text texto3;
+    float a = 0 ;
+    float b = 0;
+    float c = 0 ;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+  
     // Update is called once per frame
     void Update()
     {
-        
+        texto1.text = a.ToString();
+        a++;
+    }
+    private void FixedUpdate()
+    {
+        texto2.text = b.ToString();
+        b++;
+    }
+    private void Start()
+    {
+        StartCoroutine(Contador());
+    }
+    IEnumerator Contador()
+    {
+        yield return new WaitForSeconds(1.0f);
+
     }
 }
